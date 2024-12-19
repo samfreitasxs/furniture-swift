530979d19045c1c47cab976793663024032bc641
Samuel Freitas <37847692+samfreitasxs@users.noreply.github.com>
2024-12-19 16:45:17 -0300

    Initial Commit

---
+++ furniture-swift/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  furniture-swift
+//
+//  Created by Samuel Freitas on 19/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ furniture-swift/furniture_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  furniture_swiftApp.swift
+//  furniture-swift
+//
+//  Created by Samuel Freitas on 19/12/24.
+//
+
+import SwiftUI
+
+@main
+struct furniture_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
