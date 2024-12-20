17379b28ef8d743cced44f3cd7c01fae9a2b2584
Samuel Freitas <37847692+samfreitasxs@users.noreply.github.com>
2024-12-20 09:07:36 -0300

    include  currentTab, EdgeInsets

--- furniture-swift/ContentView.swift
+++ furniture-swift/ContentView.swift
@@ -8,17 +8,111 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var currentTab: Tab = .Home
+    
+    init() {
+        UITabBar.appearance().isHidden = true
+    }
+    
+    @Namespace var animation
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView (selection: $currentTab) {
+            
+            Text("Home View")
+            Text("Search View")
+            Text("Notifications View")
+            Text("Cart View")
+            Text("Profile View")
         }
-        .padding()
+        .overlay(
+            HStack(spacing: 0){
+                ForEach(Tab.allCases, id: \.rawValue){tab in
+                    TabButton(tab: tab)
+                }
+                .padding(.vertical)
+                .padding(.bottom, getSafeArea().bottom == 0 ? 5 : (getSafeArea().bottom - 15))
+                .background(Color("kSecondary"))
+            }
+            ,
+            alignment: .bottom
+        )
+        .ignoresSafeArea(.all, edge: .bottom)
+        
+    }
+    
+    func TabButton(tab: Tab) -> some View {
+        GeometryReader{proxy in
+            Button(action: {
+                withanimation(.spring())
+            },
+            
+                    label: {})
+            
+        }
+    }
+    
+}
+    
+    struct ContentView_Preview: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
+    }
+    
+    
+    enum Tab: String, CaseIterable {
+        case Home = "house"
+        case Search = "magnifyingglass.circle"
+        case Notifications = "bell"
+        case Cart = "bag"
+        case Profile = "person"
+        
+        var Tabname: String {
+            switch self {
+            case .Home:
+                return "Home"
+            case .Search:
+                return "Search"
+            case .Notifications:
+                return "Notifications"
+            case .Cart:
+                return "Cart"
+            case .Profile:
+                return "Profile"
+                
+        }
     }
 }
+    
+    
+extension View{
+        func (getSafeArea() -> UIEdgeInsets {
+            
+            guard let screen = UIApplication.shared.connectedScenes.first as?
+                    UIWindowScene else {
+                return .zero
+            }
+            
+            guard let safeArea = screen.windows.first?.safeAreaInsets else {
+                return .zero
+            }
+            
+            return safeArea
+        }
+    }
 
-#Preview {
-    ContentView()
+struct MaterialEffect: UIViewRepresentable {
+    var style: UIBlurEffect.Style
+    
+    func makeUIView(context: Context) -> UIVisualEffectView{
+        let view = UIVisualEffectView(effect: UIBlurEffect(style: style))
+        
+        return view
+    }
+    
+    func updateUIView(_ uiView: UIVisualEffectView, context: Context) {
+        
+    }
 }
