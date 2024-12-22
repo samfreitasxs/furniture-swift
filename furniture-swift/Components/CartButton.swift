c03caeddc74d8d076ff705155ebae67ef9d5f71c
Samuel Freitas <37847692+samfreitasxs@users.noreply.github.com>
2024-12-22 01:44:19 -0300

    include HomeView

---
+++ furniture-swift/Components/CartButton.swift
@@ -0,0 +1,31 @@
+//
+//  CartButton.swift
+//  furniture-swift
+//
+//  Created by Samuel Freitas on 22/12/24.
+//
+
+import SwiftUI
+
+struct CartButton: View {
+    var numberOfProducts: Int
+    var body: some View {
+        ZStack(alignment: .topTrailing){
+            Image(systemName: "bag.fill")
+                .foregroundColor(.black)
+                .padding(5)
+            if numberOfProducts > 0 {
+                Text("\(numberOfProducts)")
+                    .font(.caption2)
+                    .foregroundColor(.white)
+                    .frame(width: 15, height: 15)
+                    .background(.green)
+                    .cornerRadius(50)
+            }
+        }
+    }
+}
+
+#Preview {
+    CartButton(numberOfProducts: 1)
+}
---
+++ furniture-swift/Views/HomePageView.swift
@@ -0,0 +1,93 @@
+//
+//  HomePageView.swift
+//  furniture-swift
+//
+//  Created by Samuel Freitas on 22/12/24.
+//
+
+import SwiftUI
+
+struct HomePageView: View {
+    @EnvironmentObject var cartManager: CartManager
+    var body: some View {
+        NavigationStack {
+            ZStack(alignment: .top){
+                Color.white
+                    .edgesIgnoringSafeArea(.all)
+                
+                
+                VStack{
+                    AppBar()
+                    
+                    SearchView()
+                    
+                    ImageSliderView()
+                    
+                    HStack{
+                        Text("New Rivals")
+                            .font(.title2)
+                            .fontWeight(.medium)
+                        
+                        Spacer()
+                        
+                        Image(systemName: "circle.grid.2x2.fill")
+                            .foregroundColor(Color("kPrimary"))
+                    }
+                    .padding()
+                    
+                    
+                    ScrollView(.horizontal, showsIndicators: false) {
+                        HStack(spacing: 10) {
+                            ForEach(productList, id: \.id){product in
+                                NavigationLink{} label: {
+                                    ProductCardView(product: product)
+                                }
+                            }
+                        }
+                    }
+                    
+                }
+                
+            }
+        }
+    }
+}
+
+#Preview {
+    HomePageView()
+        .environmentObject(CartManager())
+}
+
+struct AppBar: View {
+    @EnvironmentObject var cartManager: CartManager
+    var body: some View {
+        NavigationStack {
+            VStack(alignment: .leading){
+                HStack{
+                    Image(systemName: "location.north.fill")
+                        .resizable()
+                        .frame(width: 20, height: 20)
+                        .padding(.trailing)
+                    
+                    Text("Maringa, Brasil")
+                        .font(.title2)
+                        .foregroundColor(.gray)
+                    
+                    Spacer()
+                    
+                    NavigationLink(destination: Text("")){
+                        CartButton(numberOfProducts: cartManager.products.count)
+                    }
+                }
+                Text( "Find The Most \nLuxurios")
+                    .font(.largeTitle .bold())
+                
+                + Text(" Furniture")
+                    .font(.largeTitle . bold())
+                    .foregroundColor(Color("kPrimary"))
+            }
+        }
+        .padding()
+        .environmentObject(CartManager())
+    }
+}
