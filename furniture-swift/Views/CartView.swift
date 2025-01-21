f9440f98d60103e25e482e622a49455cb0b965d6
Samuel Freitas <37847692+samfreitasxs@users.noreply.github.com>
2025-01-21 11:21:49 -0300

    Update cartView

--- furniture-swift/Views/CartView.swift
+++ furniture-swift/Views/CartView.swift
@@ -32,7 +32,7 @@ struct CartView: View {
                 Text("Your Cart is Empty")
             }
         }
-        .navigationTitle(Text("My Cart"))
+        .navigationTitle(Text("My Cartt"))
         .padding(.top)
     }
 }
